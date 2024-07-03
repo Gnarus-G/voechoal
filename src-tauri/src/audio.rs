35a3ad75817a7fead9e796bc82f57931a1173f13
gnarus-g <37311893+Gnarus-G@users.noreply.github.com>
2024-07-03 01:29:17 -0400

    feat: saving audio to wav file on input stream pause

--- src-tauri/src/audio.rs
+++ src-tauri/src/audio.rs
@@ -1,7 +1,12 @@
 pub mod ecouter {
-    use std::sync::mpsc::{channel, Sender};
+    use std::sync::{
+        mpsc::{channel, Sender},
+        Arc, Mutex,
+    };
 
     use cpal::traits::StreamTrait;
+
+    use crate::audio;
 
     enum Control {
         Play,
@@ -35,34 +40,47 @@ pub mod ecouter {
             .default_input_device()
             .expect("no input device available");
 
-        let mut supported_configs_range = mic
-            .supported_input_configs()
-            .expect("error while querying configs");
-
-        let supported_config = supported_configs_range
-            .next()
-            .expect("no supported config?!")
-            .with_max_sample_rate();
-
+        let supported_config = mic.default_input_config().expect("no supported config?!");
         let config = supported_config.into();
 
         let (send_ctrl, receiver_ctrl) = channel::<Control>();
 
         let _handle = std::thread::spawn(move || {
+            let audio_buffer = Arc::new(Mutex::new(vec![] as Vec<f32>));
+            let audio_buffer_ref = Arc::clone(&audio_buffer);
+
             let stream = mic
                 .build_input_stream(
                     &config,
                     move |data: &[f32], _| {
                         eprintln!("[info]: {:?}", data[0]);
+                        audio_buffer_ref
+                            .lock()
+                            .expect("failed to lock on audio_buffer")
+                            .extend(data);
                     },
                     err_fn,
                     None,
                 )
                 .expect("failed to build_input_stream");
 
-            eprintln!("[info] listen loop");
-
             stream.pause().expect("failed to pause the input stream");
+
+            let on_pause = |stream: &cpal::Stream, config: &cpal::StreamConfig| {
+                stream.pause().expect("failed to pause the input stream");
+
+                let mut writer = audio::save::WavWriter::setup(config);
+
+                {
+                    let buffer = audio_buffer.lock().expect("failed to lock on audio_buffer");
+                    writer.write_to_wav(&buffer);
+                }
+
+                audio_buffer
+                    .lock()
+                    .expect("failed to lock on audio_buffer")
+                    .clear();
+            };
 
             loop {
                 let ctrl = receiver_ctrl
@@ -71,7 +89,7 @@ pub mod ecouter {
 
                 match ctrl {
                     Control::Play => stream.play().expect("failed to play the input stream"),
-                    Control::Pause => stream.pause().expect("failed to pause the input stream"),
+                    Control::Pause => on_pause(&stream, &config),
                 };
             }
         });
@@ -81,5 +99,57 @@ pub mod ecouter {
 
     fn err_fn(err: cpal::StreamError) {
         eprintln!("[error] an error occurred on stream: {}", err);
+    }
+}
+
+mod save {
+    use std::{
+        fs,
+        path::{Path, PathBuf},
+    };
+
+    use cuid2::cuid;
+
+    pub struct WavWriter {
+        spec: hound::WavSpec,
+        save_dirpath: PathBuf,
+    }
+
+    impl WavWriter {
+        pub fn setup(config: &cpal::StreamConfig) -> Self {
+            let home_dir = std::env::var("HOME").expect("failed to resolve $HOME variable");
+            let save_dir = Path::new(&home_dir).join("voechoal");
+
+            if !save_dir.is_dir() {
+                fs::create_dir(&save_dir).expect("failed to create save dir");
+            }
+
+            let spec = hound::WavSpec {
+                channels: config.channels,
+                sample_rate: config.sample_rate.0,
+                bits_per_sample: 32,
+                sample_format: hound::SampleFormat::Float,
+            };
+
+            eprintln!("[info] wav specs: {:?}", spec);
+
+            Self {
+                spec,
+                save_dirpath: save_dir,
+            }
+        }
+
+        pub fn write_to_wav(&mut self, buffer: &[f32]) {
+            let id = cuid();
+            let wav_filepath = self.save_dirpath.join(id).with_extension("wav");
+            let mut writer = hound::WavWriter::create(wav_filepath, self.spec)
+                .expect("failed to create wav writer");
+
+            for sample in buffer.iter() {
+                writer
+                    .write_sample(*sample * 2.0)
+                    .expect("failed to write sample");
+            }
+        }
     }
 }
